a1c940487ae90cc1c41ae97a9ed5b0225e113fb9
Alexandr Vicev <saneavicev@mail.ru>
2021-06-07 11:02:53 +0300

    New feature: weather forecast for 7 days

---
+++ src/main/java/md/basarabeasca/bot/weather/service/ParseWeather.java
@@ -0,0 +1,134 @@
+package md.basarabeasca.bot.weather.service;
+
+import md.basarabeasca.bot.weather.dto.Weather;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static java.lang.Math.round;
+
+@Component
+public class ParseWeather implements JsonParser {
+
+    @Value("${weather.site}")
+    private String site;
+    @Value("${weather.appid}")
+    private String appId;
+
+    @Override
+    public String getWeather() throws IOException {
+        return parseToSendMessage(
+                Objects.requireNonNull(parseToPojoObject(
+                        readJsonFromUrl(site + appId)
+                ))
+        );
+    }
+
+    private String parseToSendMessage(List<Weather> forecast) {
+        if (forecast.isEmpty()) {
+            return "Погода недоступна. Обратитесь к администратору бота: @SaneaV";
+        } else {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (Weather weather :
+                    forecast) {
+                stringBuilder
+                        .append("Дата: *")
+                        .append(weather.getDate())
+                        .append("*\nТемпература днём: ")
+                        .append(weather.getTempDay())
+                        .append("\nТемпература ночью: ")
+                        .append(weather.getTempNight())
+                        .append("\nОписание: ")
+
+                        .append(Character
+                                .toUpperCase(weather
+                                        .getDescription()
+                                        .charAt(0)))
+                        .append(weather
+                                .getDescription()
+                                .substring(1))
+
+                        .append("\n\n");
+            }
+            return stringBuilder.toString();
+        }
+    }
+
+    private List<Weather> parseToPojoObject(JSONObject json) {
+
+        if (json.isEmpty()) {
+            return null;
+        } else {
+            JSONArray jsonArray = json.getJSONArray("daily");
+            List<Weather> forecast = new ArrayList<>();
+            final DateTimeFormatter formatter =
+                    DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+            for (Object object :
+                    jsonArray) {
+
+                JSONObject jsonObject = (JSONObject) object;
+
+                Weather weather = Weather.builder()
+                        .date(Instant
+                                .ofEpochSecond(jsonObject
+                                        .getLong("dt"))
+                                .atZone(ZoneId.of("GMT-3"))
+                                .format(formatter))
+
+                        .tempDay(String
+                                .valueOf(round(
+                                        jsonObject
+                                                .getJSONObject("temp")
+                                                .getDouble("day"))))
+
+                        .tempNight(String
+                                .valueOf(round(
+                                        jsonObject
+                                                .getJSONObject("temp")
+                                                .getDouble("night"))))
+
+                        .description(jsonObject
+                                .getJSONArray("weather")
+                                .getJSONObject(0)
+                                .getString("description"))
+                        .build();
+                forecast.add(weather);
+            }
+            return forecast;
+        }
+    }
+
+    private String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
+    public JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+        try (InputStream is = new URL(url).openStream()) {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+            String jsonText = readAll(rd);
+            return new JSONObject(jsonText);
+        }
+    }
+}
