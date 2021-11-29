3fbcddb3d4b41918fa32f34f06cfed61bd5b0ae8
Alexandr Vicev <saneavicev@mail.ru>
2021-11-29 10:13:51 +0200

    Small fix in number regex

--- src/main/java/md/basarabeasca/bot/action/callback/CallbackQueryHandler.java
+++ src/main/java/md/basarabeasca/bot/action/callback/CallbackQueryHandler.java
@@ -1,7 +1,5 @@
 package md.basarabeasca.bot.action.callback;
 
-import md.basarabeasca.bot.feature.hotnumbers.dao.model.PhoneNumber;
-import md.basarabeasca.bot.feature.hotnumbers.dto.PhoneNumberDto;
 import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 
@@ -18,20 +16,4 @@ public interface CallbackQueryHandler {
     List<? super PartialBotApiMethod<?>> handleCallbackQuery(CallbackQuery callbackQuery);
 
     CallbackQueryType getHandlerQueryType();
-
-    default StringBuilder formatPhoneNumbers(List<PhoneNumberDto> phoneNumber, StringBuilder formattedPhones) {
-        phoneNumber.forEach(
-                number -> {
-                    int i = 0;
-                    formattedPhones
-                            .append(++i)
-                            .append(POINT)
-                            .append(number.getPhoneNumber())
-                            .append(HYPHEN)
-                            .append(number.getDescription())
-                            .append(NEW_LINE);
-                }
-        );
-        return formattedPhones;
-    }
 }
